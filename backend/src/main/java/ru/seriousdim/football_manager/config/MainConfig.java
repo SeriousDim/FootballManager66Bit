ab80ce3178251568059163b372df2738c825f970
SeriousDim <dlykov2001@yandex.ru>
2023-02-02 15:20:07 +0500

    Fixing CORS

--- backend/src/main/java/ru/seriousdim/football_manager/config/MainConfig.java
+++ backend/src/main/java/ru/seriousdim/football_manager/config/MainConfig.java
@@ -3,6 +3,6 @@ package ru.seriousdim.football_manager.config;
 public class MainConfig {
 
     public static final String HEAD_ROUTE = "/catalog";
-    public static final String FRONTEND_URL = "http://127.0.0.1:5173";
+    public static final String[] FRONTEND_URLS = new String[]{"http://127.0.0.1:5173", "http://localhost:5173"};
 
 }
